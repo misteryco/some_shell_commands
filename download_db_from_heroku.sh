0092aead2cc81934416dbd430938cbfdd51dbd22
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2025-10-24 17:47:45 +0300

    Update download_db_from_heroku.sh

--- download_db_from_heroku.sh
+++ download_db_from_heroku.sh
@@ -1,4 +1,4 @@
 DB_NAME="some_name"
 
 echo "Fetching the latest database dump..."
-curl -o db.dump `heroku pg:backups:url --app app_name`
+curl -o "db_$(date +%F).dump" "$(heroku pg:backups:url --app app_name)"
