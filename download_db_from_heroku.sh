e514011313304f375d010c8a70d1687f7a06414f
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2025-10-24 17:45:44 +0300

    Update download_db_from_heroku.sh

--- download_db_from_heroku.sh
+++ download_db_from_heroku.sh
@@ -2,9 +2,3 @@ DB_NAME="some_name"
 
 echo "Fetching the latest staging database dump..."
 curl -o db.dump `heroku pg:backups:url --app app_name`
-
-echo "Recreating the local database..."
-dropdb --if-exists "$DB_NAME"
-
-echo "Creating database ..."
-sudo -u "$1" createdb -O "$1" "$DB_NAME"
