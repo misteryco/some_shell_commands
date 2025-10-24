09b37dab6ed6f413ee267dc5097692134e01f368
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2025-10-24 17:43:47 +0300

    Update and rename download_and_sync_with_heroku.sh to download_db_from_heroku.sh

--- download_db_from_heroku.sh
+++ download_db_from_heroku.sh
@@ -8,10 +8,3 @@ dropdb --if-exists "$DB_NAME"
 
 echo "Creating database ..."
 sudo -u "$1" createdb -O "$1" "$DB_NAME"
-
-echo "Starting pg_restore on the local database..."
-pg_restore -U $1 -d "$DB_NAME" -v db.dump > dump_log 2>&1
-rm db.dump
-
-echo "Running migrations on the local database..."
-python manage.py migrate
